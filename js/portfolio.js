f5a3ca739df661c73cd174fc8c0a9a49b90c309a
kmargot <margot.keller@etu.unistra.fr>
2016-06-25 16:09:02 +0200

    RESPONSIVE page accueil
    Responsive: section photo et description
    
    Ajout de codepen dans le footerr

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,27 +1,17 @@
+$(".tohide").hide();
+$(".button-web").on("click", function() {
+  var target = $(this).data("target");
+  if(target!==undefined) {
+    $(target).toggle();
+      jQuery('#graphisme').hide();
+  }
+});
 
-jQuery(document).ready(function()
-{
-     jQuery('.galerie').hide();
-    jQuery('.test').hide();
-   // On cache la zone 
-   // jQuery('.graphisme').hide();
-  //  jQuery('.integration').hide();
-   // jQuery('.autre').hide();
-//jQuery('.webdesign').hide();
-  
-    //lorsque le lien avec l'ID #.. est cliqué
-   jQuery('a.integration-link').click(function()
-  {
-       jQuery('.infographie-content').hide();
-       
-      jQuery('.test').toggle;
-      return false;
-   });
-    jQuery('a.infographie-link').click(function()
-  {
-        jQuery('.integration-content').hide();
-        
-      jQuery('.infographie-content').show(800);
-      return false;
-   });
+
+$(".button-graphisme").on("click", function() {
+  var target = $(this).data("target");
+  if(target!==undefined) {
+    $(target).toggle();
+      jQuery('#web').hide();
+  }
 });
